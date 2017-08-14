a5131d95aedabc7f30fd97a98b7a8fad162217be
nanjingzt <30795421+nanjingzt@users.noreply.github.com>
2017-08-14 14:11:26 +0800

    Create TwoSum.js

---
+++ TwoSum.js
@@ -0,0 +1,20 @@
+/*
+读完题首先想到的就是两层遍历法，但是显然时间复杂度太高，是O(N^2)，不符合要求，于是就应该想如何降低复杂度。
+首先应该想将逐个比较转变为直接查找，即首先计算出 target与当前元素的差，然后在序列中寻找这个差值，这样首先就把问题简化了。
+而寻找的过程可以先对序列进行快排，然后二分查找，这样整体的复杂度就降低为 O(N*logN) 了；
+查找最快的方法是利用一个 map容器存储每个元素的索引，这样取得某个特定元素的索引只需要常数时间即可完成，这样就更快了。
+最多只需遍历一次序列，将元素及其索引加入map中，在遍历的过程中进行对应差值的查找，如果找到了就结束遍历，这样时间复杂度最多为 O(N)
+*/
+
+		function sum(nums, target) {
+		    var exist = [];
+		    for(var i = 0; i < nums.length; i++){
+		       var tmp = target - nums[i];
+		       if (exist[tmp] !== undefined){
+		           return [exist[tmp], i];
+		       }
+		       exist[nums[i]] = i
+		    }
+		};
+		var result = sum([3,2,4],6);
+		console.log(result);
